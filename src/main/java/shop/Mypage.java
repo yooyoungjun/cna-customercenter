9e090f911d9a7a6194f651d3680d12f57cb34035
yooyoungjun <iamakira83@gmail.com>
2020-08-21 13:17:05 +0900

    initial commit

---
+++ src/main/java/shop/Mypage.java
@@ -0,0 +1,55 @@
+package shop;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="Mypage_table")
+public class Mypage {
+
+        @Id
+        @GeneratedValue(strategy=GenerationType.AUTO)
+        private Long id;
+        private Long orderId;
+        private String productId;
+        private Integer qty;
+        private String status;
+
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+        public Long getOrderId() {
+            return orderId;
+        }
+
+        public void setOrderId(Long orderId) {
+            this.orderId = orderId;
+        }
+        public String getProductId() {
+            return productId;
+        }
+
+        public void setProductId(String productId) {
+            this.productId = productId;
+        }
+        public Integer getQty() {
+            return qty;
+        }
+
+        public void setQty(Integer qty) {
+            this.qty = qty;
+        }
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+}
